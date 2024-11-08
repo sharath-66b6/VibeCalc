90099c7d5a01b035a49f87294125696591c321cc
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2024-11-08 23:11:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const backgrounds = [
     'https://images.hdqwalls.com/download/3d-render-flower-5k-ma-1336x768.jpg',
     'https://images.hdqwalls.com/download/3d-cube-art-4k-j0-1336x768.jpg'
 ];
-let currentBackgroundIndex = 0;
+let currentBackgroundIndex = 1;
 document.body.style.backgroundImage = `url(${backgrounds[currentBackgroundIndex]})`;
 
 function appendToDisplay(input) {
